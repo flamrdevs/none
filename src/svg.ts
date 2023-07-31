ca061b6787044ad124483ee153f709d778c56d2a
flamrdevs <flamrdevs@gmail.com>
2023-07-31 20:50:35 +0700

    chore(~/svg): new h utility

--- src/svg.ts
+++ src/svg.ts
@@ -67,6 +67,8 @@ type RootComponent<P extends {} = {}> = (props: P) => RootElement;
 type ImgFunction = (element: RootElement) => Promise<string>;
 type SVGFunction = (context: Context, query: Record<string, string>, element: () => RootElement) => Promise<Response>;
 
+const h = <P extends Record<string, any>>(type: Tag, props: P) => ({ type, props });
+
 const img = await (async () => {
   type FontOptions = SatoriOptions["fonts"][number];
 
@@ -127,4 +129,5 @@ const svg = await (async () => {
 })();
 
 export type { PropsWithChildren, PropsWithOptionalChildren, Element, RootElement, Component, RootComponent };
+export { h, img };
 export default svg;
