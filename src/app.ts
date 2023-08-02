5995a411f32dc08814b2812daf4dbd79f6c5a9de
flamrdevs <flamrdevs@gmail.com>
2023-08-02 20:17:16 +0700

    chore: build date

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,15 @@ import { cors } from "hono/cors";
 import { compress } from "hono/compress";
 import { logger } from "hono/logger";
 
+import dayjs from "dayjs";
 import { z } from "zod";
 
 import { hono } from "~/utils";
 import { bundlejs, npm, ui } from "~/routes";
 
 const app = hono();
+
+const build = dayjs().format("DD-MM-YYYY");
 
 app
   .use("*", cors({ origin: "*" }))
@@ -23,7 +26,7 @@ app
   .route("/ui", ui);
 
 app
-  .get("/", (ctx) => ctx.json({ name: "none" }))
+  .get("/", (ctx) => ctx.json({ name: "none", build }))
   .notFound((ctx) => ctx.json({ message: "Not found" }, 404))
   .onError((error, ctx) => {
     let status: number = 500;
