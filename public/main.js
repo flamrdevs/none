f877a0cc43891a50b57a42f852d07a83aec88021
flamrdevs <flamrdevs@gmail.com>
2023-08-08 15:32:36 +0700

    chore: update port

--- public/main.js
+++ public/main.js
@@ -2,7 +2,7 @@ import app from "./app.js";
 
 Deno.serve(
   {
-    port: Number(Deno.env.get("PORT") || 8000),
+    port: 8000,
     hostname: "0.0.0.0",
     onListen: ({ hostname, port }) => {
       console.log(`[none]: ${hostname}:${port}`);
