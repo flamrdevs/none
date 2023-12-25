400e3fc7ebd0242c2a1af52d9d117da492b7dd7a
flamrdevs <flamrdevs@gmail.com>
2023-12-26 01:11:47 +0700

    feat(libs/bundlejs): externalize deps

--- src/libs/bundlejs.ts
+++ src/libs/bundlejs.ts
@@ -2,6 +2,7 @@ import * as v from 'valibot';
 
 import * as fetchs from './fetchs';
 
+import { getPackageItem } from './npm';
 import { memo } from './utils';
 
 type BundleItem = v.Output<typeof BundleItemSchema>;
@@ -20,6 +21,16 @@ const BundleItemSchema = v.object({
 
 const loadBundleItem = memo<BundleItem>();
 
-const getBundleItem = (name: string): Promise<BundleItem> => loadBundleItem(name, async () => v.parse(BundleItemSchema, await fetchs.get.json(`https://deno.bundlejs.com/?q=${name}`)));
+const getBundleItem = (name: string): Promise<BundleItem> =>
+  loadBundleItem(name, async () => {
+    const npm = await getPackageItem(name);
+
+    const external: string[] = [];
+    if (npm.dependencies) external.push(...Object.keys(npm.dependencies));
+    if (npm.peerDependencies) external.push(...Object.keys(npm.peerDependencies));
+    const config = { esbuild: { external } };
+
+    return v.parse(BundleItemSchema, await fetchs.get.json(`https://deno.bundlejs.com/?q=${name}&config=${JSON.stringify(config)}`));
+  });
 
 export { getBundleItem };
